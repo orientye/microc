03e878eb89312cfc3fa22a1477aa99da58099f50
orientye <orientye@qq.com>
2023-06-06 12:31:31 +0000

    mcc

--- mcc.h
+++ mcc.h
@@ -1,4 +1,20 @@
 #ifndef _MCC_H
 #define _MCC_H
 
+enum LexErr {
+	kOK,
+	kERR
+};
+
+enum TokenType {
+	kIDENTIFIER,
+	kKEYWORD,
+	kOPERATOR,
+	kSYMBOL,
+	kCOMMENT,
+	kNEWLINE,
+	kNUMBER,
+	kSTRING
+};
+
 #endif
