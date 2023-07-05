39e3c34876575d25262f582c5984bdf5c38f2ac2
orientye <orientye@qq.com>
2023-07-05 14:23:04 +0000

    mcc

--- vec.h
+++ vec.h
@@ -6,4 +6,14 @@ void* vec_create(void);
 
 void vec_destory(void* vec);
 
+void vec_insert(void* vec, size_t pos);
+
+void vec_push_back(void* vec);
+
+void vec_erase(void* vec, size_t pos, size_t len);
+
+void vec_pop_back(void* vec);
+
+void vec_size(void* vec);
+
 #endif
