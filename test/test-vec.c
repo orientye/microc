cd531436dd64761705c8442fa9e9e435ea2aabcd
orientye <orientye@qq.com>
2023-07-24 14:20:44 +0000

    mcc

--- test/test-vec.c
+++ test/test-vec.c
@@ -5,14 +5,12 @@
 
 void test_vec() {
 	int* v = vec_create(int, 3);
-	vec_push_back(v, 1);
-	assert(v[0] == 1);
-	vec_push_back(v, 2);
-	assert(v[1] == 2);
-	vec_push_back(v, 3);
-	assert(v[2] == 3);
-	vec_push_back(v, 4);
-	assert(v[3] == 4);
-	vec_push_back(v, 5);
-	assert(v[4] == 5);
+	int count = 100;
+	for (int i = 0; i < count; i++) {
+		vec_push_back(v, i + 1);
+	}
+	
+	for (int i = 0; i < count; i++) {
+		assert(v[i] == (i + 1));
+	}
 }
