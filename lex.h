433f12165c9e03b0c72474ce78c9091d7c505961
orientye <orientye@qq.com>
2023-09-23 12:14:59 +0000

    mcc

--- lex.h
+++ lex.h
@@ -18,9 +18,21 @@ typedef enum {
 	kString
 } token_type_t;
 
+typedef struct {
+	int line;
+	int col;
+	const char* file_name;
+} pos_t;
 
 typedef struct {
-
+	token_type_t ty;
+	pos_t pos;
+	union {
+		char c;
+		const char* s;
+		int i;
+		void* v;
+	};
 } token_t;
 
 lex_err_t lex(const char* file_name);
