e799453952eb4cb252fba2141ee66e1a72341656
orientye <orientye@qq.com>
2023-10-01 13:01:38 +0000

    mcc

--- lex.c
+++ lex.c
@@ -1,10 +1,23 @@
 #include "lex.h"
 
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
 #define STR_EQ(str1, str2) (0 == strcmp(str1, str2))
+
+#define DIGIT_CASE \
+	case '0':        \
+	case '1':        \
+	case '2':        \
+	case '3':        \
+	case '4':        \
+	case '5':        \
+	case '6':        \
+	case '7':        \
+	case '8':        \
+	case '9'
 
 static bool is_keyword(const char* str) {
 	return  STR_EQ(str, "int") || 
@@ -21,7 +34,21 @@ static token_t* read_next_token(FILE* file) {
 	token_t* token = NULL;
 	char c = peek_char(file);
 	switch(c) {
+	DIGIT_CASE:
+		break;
 	
+	case '"':
+		break;
+	
+	case ' ':
+	case '\t':
+		break;
+	
+	case '\n':
+		break;
+	
+	default:
+		break;	
 	}
 	return NULL;
 }
